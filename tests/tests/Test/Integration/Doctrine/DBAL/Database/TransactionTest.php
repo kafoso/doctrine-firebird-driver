7946127bf6bcd9a5ca92a23197a925a9d16a438a
Kasper Søfren <Kasper.Sofren@ist.com>
2019-06-20 08:05:12 +0200

    Added test cases for UPDATE

--- tests/tests/Test/Integration/Doctrine/DBAL/Database/TransactionTest.php
+++ tests/tests/Test/Integration/Doctrine/DBAL/Database/TransactionTest.php
@@ -33,6 +33,33 @@ class TransactionTest extends AbstractIntegrationTest
         $result = $connection->query("SELECT id FROM {$tableName} WHERE id = 42");
         $value = $result->fetchColumn();
         $this->assertSame(42, $value);
+        try {
+            $connection->commit();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+    }
+
+    public function testCanSuccessfullyCommitASingleTransactionForUpdate()
+    {
+        $connection = $this->_entityManager->getConnection();
+        $tableName = strtoupper("TABLE_" . substr(md5(__CLASS__ . ':' . __FUNCTION__), 0, 12));
+        $connection->exec("CREATE TABLE {$tableName} (id INTEGER DEFAULT 0 NOT NULL)");
+        $connection->exec("INSERT INTO {$tableName} (id) VALUES (42)");
+        $connection->beginTransaction();
+        $connection->exec("UPDATE {$tableName} SET id = 43 WHERE id = 42");
+        $connection->commit();
+        $resultA = $connection->query("SELECT id FROM {$tableName} WHERE id = 42");
+        $valueA = $resultA->fetchColumn();
+        $this->assertSame(false, $valueA);
+        $resultB = $connection->query("SELECT id FROM {$tableName} WHERE id = 43");
+        $valueB = $resultB->fetchColumn();
+        $this->assertSame(43, $valueB);
+        try {
+            $connection->commit();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
     }
 
     public function testCanSuccessfullyCommitMultipleTransactionsForInsert()
@@ -65,6 +92,55 @@ class TransactionTest extends AbstractIntegrationTest
         $count = $result->fetchColumn();
         $this->assertSame(0, $connection->getTransactionNestingLevel(), "Transaction level, 1st");
         $this->assertSame(3, $count, "Count, 1st");
+        try {
+            $connection->commit();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+    }
+
+    public function testCanSuccessfullyCommitMultipleTransactionsForUpdate()
+    {
+        $map = [
+            42 => 52,
+            43 => 53,
+            44 => 54,
+        ];
+
+        $connection = $this->_entityManager->getConnection();
+        $tableName = strtoupper("TABLE_" . substr(md5(__CLASS__ . ':' . __FUNCTION__), 0, 12));
+        $connection->exec("CREATE TABLE {$tableName} (id INTEGER DEFAULT 0 NOT NULL)");
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->exec("INSERT INTO {$tableName} (id) VALUES ({$idBefore})");
+        }
+
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 42], ['ID' => 43], ['ID' => 44]], $stmt->fetchAll());
+
+        $expectedTransactionLevel = 0;
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->beginTransaction();
+            $expectedTransactionLevel++;
+            $this->assertSame($expectedTransactionLevel, $connection->getTransactionNestingLevel(), "Expected transaction level");
+            $connection->exec("UPDATE {$tableName} SET id = {$idAfter} WHERE id = {$idBefore}");
+        }
+        $stmt = $connection->query("SELECT id FROM {$tableName} WHERE id IN (" . implode(",", array_keys($map)) . ")");
+        $this->assertSame([], $stmt->fetchAll());
+
+        $expected = [['ID' => 52], ['ID' => 53], ['ID' => 54]]; // We are in inner transaction, so we see them all
+
+        $stmt = $connection->query("SELECT id FROM {$tableName} WHERE id IN (" . implode(",", $map) . ")");
+        $this->assertSame($expected, $stmt->fetchAll());
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->commit();
+            $stmt = $connection->query("SELECT id FROM {$tableName} WHERE id IN (" . implode(",", $map) . ")");
+            $this->assertSame($expected, $stmt->fetchAll());
+        }
+        try {
+            $connection->commit();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
     }
 
     public function testCanSuccessfullyRollbackASingleTransactionForInsert()
@@ -78,6 +154,37 @@ class TransactionTest extends AbstractIntegrationTest
         $result = $connection->query("SELECT id FROM {$tableName} WHERE id = 42");
         $value = $result->fetchColumn();
         $this->assertFalse($value);
+        try {
+            $connection->rollback();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+    }
+
+    public function testCanSuccessfullyRollbackASingleTransactionForUpdate()
+    {
+        $connection = $this->_entityManager->getConnection();
+        $tableName = strtoupper("TABLE_" . substr(md5(__CLASS__ . ':' . __FUNCTION__), 0, 12));
+        $connection->exec("CREATE TABLE {$tableName} (id INTEGER DEFAULT 0 NOT NULL)");
+        $connection->exec("INSERT INTO {$tableName} (id) VALUES (42)");
+
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 42]], $stmt->fetchAll());
+
+        $connection->beginTransaction();
+        $connection->exec("UPDATE {$tableName} SET id = 52 WHERE id = 42");
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52]], $stmt->fetchAll());
+        $connection->rollback();
+
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 42]], $stmt->fetchAll());
+
+        try {
+            $connection->rollback();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
     }
 
     public function testCanSuccessfullyRollbackMultipleTransactionsForInsert()
@@ -110,6 +217,58 @@ class TransactionTest extends AbstractIntegrationTest
         $count = $result->fetchColumn();
         $this->assertSame(0, $connection->getTransactionNestingLevel(), "Transaction level, 1st");
         $this->assertSame(0, $count, "Count, 1st");
+        try {
+            $connection->rollback();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+    }
+
+    public function testCanSuccessfullyRollbackMultipleTransactionsForUpdate()
+    {
+        $map = [
+            42 => 52,
+            43 => 53,
+            44 => 54,
+        ];
+
+        $connection = $this->_entityManager->getConnection();
+        $tableName = strtoupper("TABLE_" . substr(md5(__CLASS__ . ':' . __FUNCTION__), 0, 12));
+        $connection->exec("CREATE TABLE {$tableName} (id INTEGER DEFAULT 0 NOT NULL)");
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->exec("INSERT INTO {$tableName} (id) VALUES ({$idBefore})");
+        }
+
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 42], ['ID' => 43], ['ID' => 44]], $stmt->fetchAll());
+
+        $expectedTransactionLevel = 0;
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->beginTransaction();
+            $expectedTransactionLevel++;
+            $this->assertSame($expectedTransactionLevel, $connection->getTransactionNestingLevel(), "Expected transaction level");
+            $connection->exec("UPDATE {$tableName} SET id = {$idAfter} WHERE id = {$idBefore}");
+        }
+
+        $connection->rollback();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 53], ['ID' => 44]], $stmt->fetchAll());
+        $this->assertSame(2, $connection->getTransactionNestingLevel(), "Transaction level, 3rd");
+
+        $connection->rollback();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 43], ['ID' => 44]], $stmt->fetchAll());
+        $this->assertSame(1, $connection->getTransactionNestingLevel(), "Transaction level, 2nd");
+
+        $connection->rollback();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 42], ['ID' => 43], ['ID' => 44]], $stmt->fetchAll());
+        $this->assertSame(0, $connection->getTransactionNestingLevel(), "Transaction level, 1st");
+        try {
+            $connection->rollback();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
     }
 
     public function testCanSuccessfullyCommitAndRollbackMultipleTransactionsForInsert()
@@ -147,5 +306,77 @@ class TransactionTest extends AbstractIntegrationTest
         $count = $result->fetchColumn();
         $this->assertSame(0, $connection->getTransactionNestingLevel(), "Transaction level, 1st");
         $this->assertSame(1, $count, "Count, 1st");
+        try {
+            $connection->commit();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+        try {
+            $connection->rollback();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+    }
+
+    public function testCanSuccessfullyCommitAndRollbackMultipleTransactionsForUpdate()
+    {
+        $map = [
+            42 => 52,
+            43 => 53,
+            44 => 54,
+            45 => 55,
+        ];
+
+        $connection = $this->_entityManager->getConnection();
+        $tableName = strtoupper("TABLE_" . substr(md5(__CLASS__ . ':' . __FUNCTION__), 0, 12));
+        $connection->exec("CREATE TABLE {$tableName} (id INTEGER DEFAULT 0 NOT NULL)");
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->exec("INSERT INTO {$tableName} (id) VALUES ({$idBefore})");
+        }
+
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 42], ['ID' => 43], ['ID' => 44], ['ID' => 45]], $stmt->fetchAll());
+
+        $expectedTransactionLevel = 0;
+        foreach ($map as $idBefore => $idAfter) {
+            $connection->beginTransaction();
+            $expectedTransactionLevel++;
+            $this->assertSame($expectedTransactionLevel, $connection->getTransactionNestingLevel(), "Expected transaction level");
+            $connection->exec("UPDATE {$tableName} SET id = {$idAfter} WHERE id = {$idBefore}");
+        }
+
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 53], ['ID' => 54], ['ID' => 55]], $stmt->fetchAll());
+
+        $connection->rollback();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 53], ['ID' => 54], ['ID' => 45]], $stmt->fetchAll());
+        $this->assertSame(3, $connection->getTransactionNestingLevel(), "Transaction level, 4th");
+
+        $connection->commit();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 53], ['ID' => 54], ['ID' => 45]], $stmt->fetchAll());
+        $this->assertSame(2, $connection->getTransactionNestingLevel(), "Transaction level, 3rd");
+
+        $connection->rollback();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 43], ['ID' => 44], ['ID' => 45]], $stmt->fetchAll());
+        $this->assertSame(1, $connection->getTransactionNestingLevel(), "Transaction level, 2nd");
+
+        $connection->commit();
+        $stmt = $connection->query("SELECT id FROM {$tableName}");
+        $this->assertSame([['ID' => 52], ['ID' => 43], ['ID' => 44], ['ID' => 45]], $stmt->fetchAll());
+        $this->assertSame(0, $connection->getTransactionNestingLevel(), "Transaction level, 1st");
+
+        try {
+            $connection->commit();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
+        try {
+            $connection->rollback();
+        } catch (\Doctrine\DBAL\ConnectionException $e) {
+            $this->assertContains("There is no active transaction", $e->getMessage());
+        }
     }
 }
